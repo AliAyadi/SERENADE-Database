4a0f26daa5fda63948242abd6e4760a64666815c
salaou <salaou@maria-PC>
2019-11-30 17:33:44 +0100

    + Replace spinner by textfield in searchmesocosm. - Remove Ldc Class

--- src/javaNanoSerenade/MY_CONNECTION.java
+++ src/javaNanoSerenade/MY_CONNECTION.java
@@ -5,41 +5,36 @@
  */
 package javaNanoSerenade;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 /**
- *
  * @author aliay
  */
 public class MY_CONNECTION {
-    
-    
-    
-    
-    public Connection createConnection()
-    {
-        Connection connection = null;
-        
-        MysqlDataSource mds = new MysqlDataSource();
-        
-        mds.setServerName("localhost");
-        mds.setPortNumber(3306);
-        mds.setUser("root");
-        mds.setPassword("");
-        mds.setDatabaseName("java_hotel_db");
-        
-        try {
-            connection = mds.getConnection();
-        } catch (SQLException ex) {
-            Logger.getLogger(MY_CONNECTION.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return connection;
-    }
-    
-        
+
+	public Connection createConnection() {
+		Connection connection = null;
+
+		MysqlDataSource mds = new MysqlDataSource();
+
+		mds.setServerName("localhost");
+		mds.setPortNumber(3306);
+		mds.setUser("salaou");
+		mds.setPassword("");
+		mds.setDatabaseName("java_hotel_db");
+
+		try {
+			connection = mds.getConnection();
+		} catch (SQLException ex) {
+			Logger.getLogger(MY_CONNECTION.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		return connection;
+	}
+
 }
